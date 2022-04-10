c709a5ac6f74b3577d12a090d9de48fc8b61ddda
Tinywan <756684177@qq.com>
2022-04-11 00:24:16 +0800

    移除命名空间

--- src/Facade/Validate.php
+++ src/Facade/Validate.php
@@ -4,63 +4,72 @@
  * @author Tinywan(ShaoBo Wan)
  * @date 2022/02/17 20:26
  */
-
 declare (strict_types = 1);
 
 namespace Tinywan\Validate\Facade;
 
 /**
  * @see \Tinywan\Validate\Validate
+ * @package \Tinywan\Validate\Facade
  * @mixin \Tinywan\Validate\Validate
- * @method \Tinywan\Validate\Validate rule(mixed $name, mixed $rule = '') static 添加字段验证规则
- * @method void extend(string $type, callable $callback = null, string $message='') static 注册扩展验证（类型）规则
- * @method void setTypeMsg(mixed $type, string $msg = null) static 设置验证规则的默认提示信息
- * @method \Tinywan\Validate\Validate message(mixed $name, string $message = '') static 设置提示信息
- * @method \Tinywan\Validate\Validate scene(string $name) static 设置验证场景
- * @method bool hasScene(string $name) static 判断是否存在某个验证场景
- * @method \Tinywan\Validate\Validate batch(bool $batch = true) static 设置批量验证
- * @method \Tinywan\Validate\Validate only(array $fields) static 指定需要验证的字段列表
- * @method \Tinywan\Validate\Validate remove(mixed $field, mixed $rule = true) static 移除某个字段的验证规则
- * @method \Tinywan\Validate\Validate append(mixed $field, mixed $rule = null) static 追加某个字段的验证规则
- * @method bool confirm(mixed $value, mixed $rule, array $data = [], string $field = '') static 验证是否和某个字段的值一致
- * @method bool different(mixed $value, mixed $rule, array $data = []) static 验证是否和某个字段的值是否不同
- * @method bool egt(mixed $value, mixed $rule, array $data = []) static 验证是否大于等于某个值
- * @method bool gt(mixed $value, mixed $rule, array $data = []) static 验证是否大于某个值
- * @method bool elt(mixed $value, mixed $rule, array $data = []) static 验证是否小于等于某个值
- * @method bool lt(mixed $value, mixed $rule, array $data = []) static 验证是否小于某个值
- * @method bool eq(mixed $value, mixed $rule) static 验证是否等于某个值
- * @method bool must(mixed $value, mixed $rule) static 必须验证
- * @method bool is(mixed $value, mixed $rule, array $data = []) static 验证字段值是否为有效格式
- * @method bool ip(mixed $value, mixed $rule) static 验证是否有效IP
- * @method bool requireIf(mixed $value, mixed $rule) static 验证某个字段等于某个值的时候必须
- * @method bool requireCallback(mixed $value, mixed $rule,array $data) static 通过回调方法验证某个字段是否必须
- * @method bool requireWith(mixed $value, mixed $rule, array $data) static 验证某个字段有值的情况下必须
- * @method bool filter(mixed $value, mixed $rule) static 使用filter_var方式验证
- * @method bool in(mixed $value, mixed $rule) static 验证是否在范围内
- * @method bool notIn(mixed $value, mixed $rule) static 验证是否不在范围内
- * @method bool between(mixed $value, mixed $rule) static between验证数据
- * @method bool notBetween(mixed $value, mixed $rule) static 使用notbetween验证数据
- * @method bool length(mixed $value, mixed $rule) static 验证数据长度
- * @method bool max(mixed $value, mixed $rule) static 验证数据最大长度
- * @method bool min(mixed $value, mixed $rule) static 验证数据最小长度
- * @method bool after(mixed $value, mixed $rule) static 验证日期
- * @method bool before(mixed $value, mixed $rule) static 验证日期
- * @method bool expire(mixed $value, mixed $rule) static 验证有效期
- * @method bool allowIp(mixed $value, mixed $rule) static 验证IP许可
- * @method bool denyIp(mixed $value, mixed $rule) static 验证IP禁用
- * @method bool regex(mixed $value, mixed $rule) static 使用正则验证数据
- * @method bool token(mixed $value, mixed $rule) static 验证表单令牌
- * @method bool dateFormat(mixed $value, mixed $rule) static 验证时间和日期是否符合指定格式
- * @method bool unique(mixed $value, mixed $rule, array $data = [], string $field = '') static 验证是否唯一
- * @method bool check(array $data, mixed $rules = []) static 数据自动验证
- * @method bool checkRule(mixed $data, mixed $rules = []) static 数据手动验证
- * @method mixed getError() static 获取错误信息
+ * @method static \Tinywan\Validate\Validate rule(string|array $name, mixed $rule = '') 添加字段验证规则
+ * @method static \Tinywan\Validate\Validate extend(string $type, callable $callback = null, string $message = null) 注册验证（类型）规则
+ * @method static void setTypeMsg(string|array $type, string $msg = null) 设置验证规则的默认提示信息
+ * @method static Validate message(array $message) 设置提示信息
+ * @method static \Tinywan\Validate\Validate scene(string $name) 设置验证场景
+ * @method static bool hasScene(string $name) 判断是否存在某个验证场景
+ * @method static \Tinywan\Validate\Validate batch(bool $batch = true) 设置批量验证
+ * @method static \Tinywan\Validate\Validate failException(bool $fail = true) 设置验证失败后是否抛出异常
+ * @method static \Tinywan\Validate\Validate only(array $fields) 指定需要验证的字段列表
+ * @method static \Tinywan\Validate\Validate remove(string|array $field, mixed $rule = null) 移除某个字段的验证规则
+ * @method static \Tinywan\Validate\Validate append(string|array $field, mixed $rule = null) 追加某个字段的验证规则
+ * @method static bool check(array $data, array $rules = []) 数据自动验证
+ * @method static bool checkRule(mixed $value, mixed $rules) 根据验证规则验证数据
+ * @method static bool confirm(mixed $value, mixed $rule, array $data = [], string $field = '') 验证是否和某个字段的值一致
+ * @method static bool different(mixed $value, mixed $rule, array $data = []) 验证是否和某个字段的值是否不同
+ * @method static bool egt(mixed $value, mixed $rule, array $data = []) 验证是否大于等于某个值
+ * @method static bool gt(mixed $value, mixed $rule, array $data = []) 验证是否大于某个值
+ * @method static bool elt(mixed $value, mixed $rule, array $data = []) 验证是否小于等于某个值
+ * @method static bool lt(mixed $value, mixed $rule, array $data = []) 验证是否小于某个值
+ * @method static bool eq(mixed $value, mixed $rule) 验证是否等于某个值
+ * @method static bool must(mixed $value, mixed $rule = null) 必须验证
+ * @method static bool is(mixed $value, string $rule, array $data = []) 验证字段值是否为有效格式
+ * @method static bool token(mixed $value, mixed $rule, array $data) 验证表单令牌
+ * @method static bool activeUrl(mixed $value, mixed $rule = 'MX') 验证是否为合格的域名或者IP 支持A，MX，NS，SOA，PTR，CNAME，AAAA，A6， SRV，NAPTR，TXT 或者 ANY类型
+ * @method static bool ip(mixed $value, mixed $rule = 'ipv4') 验证是否有效IP
+ * @method static bool fileExt(mixed $file, mixed $rule) 验证上传文件后缀
+ * @method static bool fileMime(mixed $file, mixed $rule) 验证上传文件类型
+ * @method static bool fileSize(mixed $file, mixed $rule) 验证上传文件大小
+ * @method static bool image(mixed $file, mixed $rule) 验证图片的宽高及类型
+ * @method static bool dateFormat(mixed $value, mixed $rule) 验证时间和日期是否符合指定格式
+ * @method static bool unique(mixed $value, mixed $rule, array $data = [], string $field = '') 验证是否唯一
+ * @method static bool filter(mixed $value, mixed $rule) 使用filter_var方式验证
+ * @method static bool requireIf(mixed $value, mixed $rule, array $data = []) 验证某个字段等于某个值的时候必须
+ * @method static bool requireCallback(mixed $value, mixed $rule, array $data = []) 通过回调方法验证某个字段是否必须
+ * @method static bool requireWith(mixed $value, mixed $rule, array $data = []) 验证某个字段有值的情况下必须
+ * @method static bool requireWithout(mixed $value, mixed $rule, array $data = []) 验证某个字段没有值的情况下必须
+ * @method static bool in(mixed $value, mixed $rule) 验证是否在范围内
+ * @method static bool notIn(mixed $value, mixed $rule) 验证是否不在某个范围
+ * @method static bool between(mixed $value, mixed $rule) between验证数据
+ * @method static bool notBetween(mixed $value, mixed $rule) 使用notbetween验证数据
+ * @method static bool length(mixed $value, mixed $rule) 验证数据长度
+ * @method static bool max(mixed $value, mixed $rule) 验证数据最大长度
+ * @method static bool min(mixed $value, mixed $rule) 验证数据最小长度
+ * @method static bool after(mixed $value, mixed $rule, array $data = []) 验证日期
+ * @method static bool before(mixed $value, mixed $rule, array $data = []) 验证日期
+ * @method static bool afterWith(mixed $value, mixed $rule, array $data = []) 验证日期
+ * @method static bool beforeWith(mixed $value, mixed $rule, array $data = []) 验证日期
+ * @method static bool expire(mixed $value, mixed $rule) 验证有效期
+ * @method static bool allowIp(mixed $value, mixed $rule) 验证IP许可
+ * @method static bool denyIp(mixed $value, mixed $rule) 验证IP禁用
+ * @method static bool regex(mixed $value, mixed $rule) 使用正则验证数据
+ * @method static array|string getError() 获取错误信息
+ * @method static bool __call(string $method, array $args) 动态方法 直接调用is方法进行验证
  */
 class Validate
 {
-
     /**
-     * @var \Tinywan\Validate\Validate
+     * @var \Tinywan\Validate\Validate | null $_instance
      */
     protected static $_instance = null;
 
@@ -71,6 +80,6 @@ class Validate
      */
     public static function __callStatic($name, $arguments)
     {
-        return static::$_instance()->{$name}(...$arguments);
+        return static::$_instance->{$name}(...$arguments);
     }
 }
