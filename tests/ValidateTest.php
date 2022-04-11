50303fe5049fadbf1c629c54246881d0fc9d38cc
万少波 <756684177@qq.com>
2022-04-11 17:15:49 +0800

    测试案例

--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -13,7 +13,11 @@ use PHPUnit\Framework\TestCase;
 
 class ValidateTest extends  TestCase
 {
-    public function test(){
+    /**
+     * @desc: 基础测试
+     */
+    public function testBasic()
+    {
         $data = [
             'name'  => 'Tinywan',
             'age'  => 24,
@@ -22,5 +26,36 @@ class ValidateTest extends  TestCase
         $validate = new UserValidate();
         self::assertIsBool($validate->check($data));
         self::assertTrue($validate->check($data));
+    }
+
+    /**
+     * @desc: 助手函数测试
+     */
+    public function testHelper()
+    {
+        $data = [
+            'name'  => 'Tinywan',
+            'age'  => 24,
+            'email' => 'Tinywan@163.com'
+        ];
+        self::assertIsBool(validate($data, UserValidate::class));
+        self::assertFalse(validate($data, UserValidate::class));
+    }
+
+    /**
+     * @desc: 助手面板
+     */
+    public function testFacade()
+    {
+        $validate = \Tinywan\Validate\Facade\Validate::rule('age', 'number|between:1,120')
+            ->rule([
+                'name'  => 'require|max:25',
+                'email' => 'email'
+            ]);
+        $data = [
+            'name'  => 'tinywan',
+            'email' => 'tinywan@gmail.com'
+        ];
+        self::assertIsBool($validate->check($data));
     }
 }
